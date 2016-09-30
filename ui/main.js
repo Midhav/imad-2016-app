99d10f4e328ae2e67bc92bc70655cdf6c69802cf
Midhav <midhav_r@yahoo.com>
2016-09-30 20:22:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
 console.log('Loaded!');
+var counter = 0;
+button.onclick = function () {
+    //CREATE REQ OBJECT
+    var request = new XMLHttpRequest();
+
+    //capture the response and store in a variable
+    request.onreadystatechange = function (){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                    counter = counter + 1;
+    var span = document.getElementById('count')l
+    span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //make the request
+    request.open('GET'.'http://midhav.imad.hasura-app.io/counter', true);
+    request.send(null);
+    
+}
